37a845f80950c2f4e6ec2bf688159297e23149f0
GongZichao <3048269385@qq.com>
2026-03-05 20:19:06 +0800

    Initial Commit

---
+++ LilyAisland/LilyAislandApp.swift
@@ -0,0 +1,17 @@
+//
+//  LilyAislandApp.swift
+//  LilyAisland
+//
+//  Created by gongzichao on 2026/3/5.
+//
+
+import SwiftUI
+
+@main
+struct LilyAislandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
